bdaebd5c82101727f2fc8ad1b469dea0fca1f385
Ossamoon <osamu950623@gmail.com>
2021-03-24 23:33:29 +0900

    Learn about ViewPager2 and FragmentStateAdapter

---
+++ MySlideshow/app/src/main/java/com/example/myslideshow/ImageFragment.kt
@@ -0,0 +1,71 @@
+package com.example.myslideshow
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myslideshow.databinding.FragmentImageBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val IMG_RES_ID = "IMG_RES_ID"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ImageFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ImageFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var imageResId: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            imageResId = it.getInt(IMG_RES_ID)
+        }
+    }
+
+    private var _binding: FragmentImageBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentImageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ImageFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(imageResId: Int) =
+            ImageFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(IMG_RES_ID, imageResId)
+                }
+            }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        imageResId?.let {
+            binding.imageView.setImageResource(it)
+        }
+    }
+}
